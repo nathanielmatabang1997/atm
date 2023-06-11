06ab0c546bf58addfa17a9df3f3713aa7a721c97
Nathaniel Ursua Matabang <120173148+nathanielmatabang1997@users.noreply.github.com>
2023-06-11 18:04:37 +0800

    Add files via upload

---
+++ CALCULATOR JAVA.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class CALCULATOR JAVA {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = input.nextDouble();
+        System.out.print("Enter the second number: ");
+        double num2 = input.nextDouble();
+        System.out.print("Enter the operation (+, -, *, /): ");
+        char operator = input.next().charAt(0);
+
+        double result;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Invalid operator");
+                return;
+        }
+
+        System.out.println("The result is: " + result);
+    }
+}
